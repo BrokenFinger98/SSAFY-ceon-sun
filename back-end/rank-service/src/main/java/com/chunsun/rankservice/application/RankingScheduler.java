b03b37be6e54e8db1098ad00682aa290843d064b
김범준 <kbj7565@gmail.com>
2025-02-16 23:07:14 +0900

    Feature/118 Rank redis 데이터 유실 확인로직 추가

--- back-end/rank-service/src/main/java/com/chunsun/rankservice/application/RankingScheduler.java
+++ back-end/rank-service/src/main/java/com/chunsun/rankservice/application/RankingScheduler.java
@@ -1,8 +1,12 @@
 package com.chunsun.rankservice.application;
 
+import java.time.Instant;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.chunsun.rankservice.application.service.RankingService;
 
@@ -14,6 +18,9 @@ import lombok.RequiredArgsConstructor;
 public class RankingScheduler {
 
 	private final RankingService rankingService;
+	private final RedisTemplate<String, String> redisTemplate;
+
+	private final Instant serverStartTime = Instant.now();
 
 	@Scheduled(cron = "0 0 0/3 * * *")
 	public void mergeRealTimeViewsAndClass() {
@@ -22,9 +29,24 @@ public class RankingScheduler {
 	}
 
 	@Scheduled(cron = "0 10 3 * * *")
+	@Transactional
 	public void mergeRedisAndDB() {
 
 		rankingService.updateDatabaseRankingPoints();
 		rankingService.syncRedisRankingPoints();
+	}
+
+	@Scheduled(fixedRate = 60000)
+	public void checkRedisRanking() {
+
+		if(Instant.now().isBefore(serverStartTime.plusSeconds(10800))) return;
+
+		boolean exists = redisTemplate.hasKey("teacher:score");
+
+		if(!exists) {
+			System.out.println("Redis 랭킹 데이터 유실");
+
+			rankingService.syncRedisRankingPoints();
+		}
 	}
 }
