d2c2d0f37f1c57b51a2f8f58a90e979bc9300f99
문인규 <ansdlsrb1121@naver.com>
2025-02-27 14:11:54 +0900

    Revert "Merge branch 'test/prometheus' into 'production-BE'"
    This reverts merge request !194

--- back-end/gateway-service/src/main/java/com/chunsun/gatewayservice/config/SecurityConfig.java
+++ back-end/gateway-service/src/main/java/com/chunsun/gatewayservice/config/SecurityConfig.java
@@ -33,12 +33,11 @@ public class SecurityConfig {
 	private final JwtProvider jwtProvider;
 
 	private static final String[] PUBLIC_PATHS = {
-		"/**"
-		// "/",
-		// "favicon.ico",
-		// "/auth-service/**",
-		// "/discovery-service/**",
-		// "/actuator/prometheus"    // ✅ Prometheus 접근 허용
+		"/",
+		"favicon.ico",
+		"/auth-service/**",
+		"/discovery-service/**",
+		"/actuator/prometheus"    // ✅ Prometheus 접근 허용
 	};
 
 
